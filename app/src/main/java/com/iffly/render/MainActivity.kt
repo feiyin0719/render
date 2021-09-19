3dc52ef0435043af19290ddf1969f4a9cf657e50
iffly <582816317@qq.com>
2021-09-19 10:30:21 +0800

    change render to other thread

--- app/src/main/java/com/iffly/render/MainActivity.kt
+++ app/src/main/java/com/iffly/render/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.iffly.render
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Matrix
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.iffly.render.databinding.ActivityMainBinding
@@ -15,20 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = "123"
-
-        val render = Render(600, 600)
-        render.lock()
+        binding.sampleText.text = "render"
+        Thread {
+            val render: Render = Render(600, 600)
+            render.lock()
 //        render.line(10, 10, 100, 200, 0xffff0000)
 //        render.line(10, 10, 200, 100, 0xff0000ff)
 //        render.line(10, 10, 10, 100, 0xff00ffff)
 //        render.line(10, 10, 100, 10, 0xffff00ff)
 //        render.triangle(10, 10, 30, 10, 30, 50, 0xffff0000)
-        render.renderObject(assetManager = assets, fileName = "african_head.obj")
-        render.unlock()
+            render.renderObject(assetManager = assets, fileName = "african_head.obj")
+            render.unlock()
+            val bitmap = Bitmap.createBitmap(
+                render.bitmap.width,
+                render.bitmap.height,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            val matrix = Matrix()
 
-        binding.imageView.setImageBitmap(render.bitmap)
-        render.destroy()
+            matrix.postRotate(180f, bitmap.width / 2f, bitmap.height / 2f)
+            canvas.drawBitmap(render.bitmap, matrix, null)
+            binding.imageView.post {
+                binding.imageView.setImageBitmap(bitmap)
+            }
+            render.destroy()
+
+        }.start()
+
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
     }
 
 
